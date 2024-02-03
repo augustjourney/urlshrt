b3983fea8badb50fa553a2ae8be7984c3bed8006
Konstantin Voronoy <k.voronoy@xsolla.com>
2024-02-03 14:29:02 +0600

    feat: created http server with urlHandler

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func urlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" && r.Method == http.MethodPost {
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("http://localhost:8080/EwHXdJfB"))
+	} else if r.URL.Path != "/" && r.Method == http.MethodGet {
+		w.Header().Add("Location", "https://practicum.yandex.ru/")
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Write([]byte("https://practicum.yandex.ru/"))
+	} else {
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", urlHandler)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
