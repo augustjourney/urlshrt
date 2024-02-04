72d2c0aa7d182df1f035b0b7c87f0a394642eb55
Konstantin Voronoy <k.voronoy@xsolla.com>
2024-02-04 19:11:45 +0600

    feat: added repo, service and controller

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,19 +1,115 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 )
 
-func urlHandler(w http.ResponseWriter, r *http.Request) {
+const BASE_URL = "http://localhost:8080"
+
+type Url struct {
+	Short    string
+	Original string
+}
+
+// Storage
+
+type Repo struct{}
+
+var repo Repo
+
+var URLS_IN_MEMORY []Url
+
+func (r *Repo) Create(short string, original string) {
+	url := Url{
+		Short:    short,
+		Original: original,
+	}
+	URLS_IN_MEMORY = append(URLS_IN_MEMORY, url)
+}
+
+func (r *Repo) Get(short string) *Url {
+
+	for i := 0; i < len(URLS_IN_MEMORY); i++ {
+		if URLS_IN_MEMORY[i].Short == short {
+			return &URLS_IN_MEMORY[i]
+		}
+	}
+
+	return nil
+}
+
+// Service, main logic
+
+type Service struct {
+	repo Repo
+}
+
+var service Service
+
+func (s *Service) Shorten(originalUrl string) string {
+	short := "EwHXdJfB"
+	s.repo.Create(short, originalUrl)
+	return BASE_URL + "/" + short
+}
+
+func (s *Service) FindOriginal(short string) (error, string) {
+	url := s.repo.Get(short)
+	if url == nil {
+		return errors.New("Url not found"), ""
+	}
+	return nil, url.Original
+}
+
+// Http Controller
+
+type Controller struct {
+	service Service
+}
+
+func (c *Controller) urlHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" && r.Method == http.MethodPost {
+		// Getting url from text plain body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		originalUrl := string(body)
+
+		// Make a short url
+		short := c.service.Shorten(originalUrl)
+
+		// Response
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("http://localhost:8080/EwHXdJfB"))
+		w.Write([]byte(short))
+		return
 	} else if r.URL.Path != "/" && r.Method == http.MethodGet {
-		w.Header().Add("Location", "https://practicum.yandex.ru/")
+
+		// Parse short url
+		short := r.URL.Path[1:]
+
+		// Find original
+		err, originalUrl := c.service.FindOriginal(short)
+
+		fmt.Println("originalUrl", originalUrl)
+
+		if err != nil {
+			// Response
+			fmt.Println(err)
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Response
+		w.Header().Add("Location", originalUrl)
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusTemporaryRedirect)
-		w.Write([]byte("https://practicum.yandex.ru/"))
+		w.Write([]byte(originalUrl))
+		return
 	} else {
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusBadRequest)
@@ -23,8 +119,23 @@ func urlHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", urlHandler)
+
+	URLS_IN_MEMORY = make([]Url, 0)
+
+	repo = Repo{}
+
+	service = Service{
+		repo: repo,
+	}
+
+	c := Controller{
+		service: service,
+	}
+
+	mux.HandleFunc("/", c.urlHandler)
+
 	err := http.ListenAndServe(":8080", mux)
+
 	if err != nil {
 		panic(err)
 	}
