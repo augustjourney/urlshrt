a07704ad6f3523ed2c43cd2e3415cab9be3b00a5
Konstantin Voronoy <k.voronoy@xsolla.com>
2024-02-23 14:55:15 +0600

    feat: created request logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,12 @@
 package logger
 
-import "github.com/sirupsen/logrus"
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+)
 
 var Log *logrus.Logger
 
@@ -8,4 +14,13 @@ func New() *logrus.Logger {
 	Log = logrus.New()
 	Log.SetLevel(logrus.InfoLevel)
 	return Log
+}
+
+func RequestLogger(ctx *fiber.Ctx) error {
+	start := time.Now()
+	result := ctx.Next()
+	duration := time.Since(start).Milliseconds()
+	log := fmt.Sprintf("HTTP Request – Method: %s, Path: %s, Duration: %dms, Status: %d, ContentLength: %d", ctx.Method(), ctx.OriginalURL(), duration, ctx.Response().StatusCode(), len(ctx.Response().Body()))
+	Log.Info(log)
+	return result
 }
