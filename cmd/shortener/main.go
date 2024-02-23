b2699ed4f00dd8bc0addc8858f990eefd2bc567f
Konstantin Voronoy <k.voronoy@xsolla.com>
2024-02-23 13:15:28 +0600

    chore: installed logrus, set up logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/augustjourney/urlshrt/internal/app"
 	"github.com/augustjourney/urlshrt/internal/config"
 	"github.com/augustjourney/urlshrt/internal/controller"
+	"github.com/augustjourney/urlshrt/internal/logger"
 	"github.com/augustjourney/urlshrt/internal/service"
 	"github.com/augustjourney/urlshrt/internal/storage/inmemory"
 )
@@ -13,13 +14,14 @@ import (
 func main() {
 
 	config := config.New()
+	logger.New()
 
 	repo := inmemory.New()
 	service := service.New(&repo, config)
 	controller := controller.New(&service)
 	app := app.New(&controller)
 
-	fmt.Printf("Launching on %s", config.ServerAddress)
+	logger.Log.Info(fmt.Sprintf("Launching on %s", config.ServerAddress))
 
 	err := app.Listen(config.ServerAddress)
 	if err != nil {
