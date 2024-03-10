8512bc8f08238ba16792a57c44a5c21f8b59ab29
Konstantin Voronoy <k.voronoy@xsolla.com>
2024-03-10 13:18:55 +0600

    feat: added postgres connection in main function

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/augustjourney/urlshrt/internal/app"
 	"github.com/augustjourney/urlshrt/internal/config"
 	"github.com/augustjourney/urlshrt/internal/controller"
+	"github.com/augustjourney/urlshrt/internal/infra"
 	"github.com/augustjourney/urlshrt/internal/logger"
 	"github.com/augustjourney/urlshrt/internal/service"
 	"github.com/augustjourney/urlshrt/internal/storage/infile"
@@ -16,14 +16,21 @@ func main() {
 	config := config.New()
 	logger.New()
 
+	db, err := infra.InitPostgres(config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
 	repo := infile.New(config)
 	service := service.New(&repo, config)
 	controller := controller.New(&service)
-	app := app.New(&controller)
+	app := app.New(&controller, db)
 
 	logger.Log.Info(fmt.Sprintf("Launching on %s", config.ServerAddress))
 
-	err := app.Listen(config.ServerAddress)
+	err = app.Listen(config.ServerAddress)
 	if err != nil {
 		panic(err)
 	}
