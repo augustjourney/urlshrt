be7b9bce892769e1330f26a17c24adcd1919b513
Konstantin Voronoy <k.voronoy@xsolla.com>
2024-03-23 18:31:54 +0600

    feat: added method to get user urls, shorten method creates url with user id

--- internal/service/service.go
+++ internal/service/service.go
@@ -22,9 +22,11 @@ type Service struct {
 }
 
 type IService interface {
-	Shorten(originalURL string) (*ShortenResult, error)
+	Shorten(originalURL string, userUUID string) (*ShortenResult, error)
 	FindOriginal(short string) (string, error)
 	ShortenBatch(batchURLs []BatchURL) ([]BatchResultURL, error)
+	GenerateID() (string, error)
+	GetUserURLs(ctx context.Context, userUUID string) (*[]storage.URL, error)
 }
 
 type ShortenResult struct {
@@ -42,7 +44,7 @@ type BatchResultURL struct {
 	CorrelationID string `json:"correlation_id"`
 }
 
-func (s *Service) generateID() (string, error) {
+func (s *Service) GenerateID() (string, error) {
 	uuid, err := uuid.NewRandom()
 
 	if err != nil {
@@ -63,9 +65,9 @@ func (s *Service) buildShortURL(short string) string {
 	return s.config.BaseURL + "/" + short
 }
 
-func (s *Service) Shorten(originalURL string) (*ShortenResult, error) {
+func (s *Service) Shorten(originalURL string, userUUID string) (*ShortenResult, error) {
 	short := s.hashURL(originalURL)
-	uuid, err := s.generateID()
+	uuid, err := s.GenerateID()
 	result := ShortenResult{
 		ResultURL:     "",
 		AlreadyExists: false,
@@ -78,6 +80,7 @@ func (s *Service) Shorten(originalURL string) (*ShortenResult, error) {
 		UUID:     uuid,
 		Short:    short,
 		Original: originalURL,
+		UserUUID: userUUID,
 	})
 
 	if err != nil {
@@ -119,7 +122,7 @@ func (s *Service) ShortenBatch(batchURLs []BatchURL) ([]BatchResultURL, error) {
 
 		short := s.hashURL(url.OriginalURL)
 
-		uuid, err := s.generateID()
+		uuid, err := s.GenerateID()
 
 		if err != nil {
 			return nil, err
@@ -156,6 +159,14 @@ func (s *Service) FindOriginal(short string) (string, error) {
 		return "", errNotFound
 	}
 	return url.Original, nil
+}
+
+func (s *Service) GetUserURLs(ctx context.Context, userUUID string) (*[]storage.URL, error) {
+	urls, err := s.repo.GetByUserUUID(ctx, userUUID)
+	if err != nil {
+		return nil, errInternalError
+	}
+	return urls, nil
 }
 
 func New(repo storage.IRepo, config *config.Config) Service {
