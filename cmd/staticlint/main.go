b4c1b44af79c26a5a8ce50fbacb47fbc4473ab4f
Kostya <53155077+augustjourney@users.noreply.github.com>
2024-06-11 21:12:53 +0300

    feat: added staticlint multichecker

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"honnef.co/go/tools/staticcheck"
+	"strings"
+)
+
+func main() {
+	var checks []*analysis.Analyzer
+
+	for _, v := range staticcheck.Analyzers {
+		// добавляем все проверки, которые начинаются на SA
+		if strings.HasPrefix(v.Analyzer.Name, "SA") {
+			checks = append(checks, v.Analyzer)
+		}
+
+		// добавляем проверку ST1012
+		// Poorly chosen name for error variable
+		if v.Analyzer.Name == "ST1012" {
+			checks = append(checks, v.Analyzer)
+		}
+
+		// добавляем проверку ST1013
+		// Should use constants for HTTP error codes, not magic numbers
+		// частая привычка писать цифры, вместо использования констант
+		if v.Analyzer.Name == "ST1013" {
+			checks = append(checks, v.Analyzer)
+		}
+	}
+
+	// добавляем дефолтные анализаторы из модуля passes
+	checks = append(checks, printf.Analyzer, shadow.Analyzer, structtag.Analyzer)
+
+	// запускаем мультичекер
+	multichecker.Main(checks...)
+}
