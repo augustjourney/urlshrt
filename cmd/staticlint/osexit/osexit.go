98c98a41dfc9da6b231809a664b0a5843e22431d
Kostya <53155077+augustjourney@users.noreply.github.com>
2024-06-12 19:04:09 +0300

    feat: added ast walking and checking for os exit

--- cmd/staticlint/osexit/osexit.go
+++ cmd/staticlint/osexit/osexit.go
@@ -1,6 +1,7 @@
 package osexit
 
 import (
+	"go/ast"
 	"golang.org/x/tools/go/analysis"
 )
 
@@ -12,14 +13,34 @@ var OsExitChecker = &analysis.Analyzer{
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	packageName := pass.Pkg.Name()
+
+	// проверяем только пакет main
 	if packageName != "main" {
 		return nil, nil
 	}
 
 	for _, file := range pass.Files {
+		// проверяем только файл main
 		if file.Name.String() != "main" {
 			continue
 		}
+
+		ast.Inspect(file, func(n ast.Node) bool {
+			callExpr, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			if fn, ok := callExpr.Fun.(*ast.SelectorExpr); ok {
+				if ident, ok := fn.X.(*ast.Ident); ok {
+					if ident.Name == "os" && fn.Sel.Name == "Exit" {
+						pass.Reportf(callExpr.Pos(), "should not use os.Exit in main")
+					}
+				}
+			}
+
+			return true
+		})
 
 	}
 
